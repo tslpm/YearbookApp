90a001361e4d8f266ed21818ded631d1e2e8b6e0
Raghu Betina <rbetina@gmail.com>
2013-01-21 15:37:54 -0600

    Added initialize and Student.fetch methods to the Student class. Beginning to refactor views.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,20 +1,32 @@
+require 'open-uri'
+
 class Student
   attr_accessor "name"
   attr_accessor "photo_url"
   attr_accessor "section"
   attr_accessor "twitter"
 
-  def introduce
-    if @twitter == ""
-      return "      <li>
-                      <img src='#{@photo_url}'>
-                      <h3>#{@name}</h3>
-                    </li>"
-    else
-      return "      <li>
-                      <img src='#{@photo_url}'>
-                      <h3><a href='https://twitter.com/#{@twitter}'>#{@name}</a></h3>
-                    </li>"
+  def Student.fetch(section)
+    url = "http://tslwebdev.herokuapp.com/#{section}.json"
+    json_string = open(url).read
+
+    student_hashes = JSON.parse(json_string)
+
+    array_of_student_objects = Array.new
+    student_hashes.each do |student_hash|
+      s = Student.new(student_hash["name"],
+                      student_hash["twitter"],
+                      student_hash["photo_url"],
+                      student_hash["section"])
+      array_of_student_objects << s
     end
+    return array_of_student_objects
+  end
+
+  def initialize(the_name, handle, pic_url, the_section)
+    @name = the_name
+    @twitter = handle
+    @photo_url = pic_url
+    @section = the_section
   end
 end
